47ec139b41f108bb0f9a3ff39a8b7ed05c284f6d
Rishi Ranajan <67317269+RiH-137@users.noreply.github.com>
2023-03-29 00:11:27 +0530

    Create BirthdayActivity.kt

---
+++ BirthdayActivity.kt
@@ -0,0 +1,29 @@
+package com.example.capt_rih
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class BirthdayActivity : AppCompatActivity() {
+    companion object{                                   //companion object just create a static variable
+
+      const val NAME_EXTRA ="name_extra"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday)
+
+       /* In general we use -->  val name  =intent.getStringExtra("name") <-- and here "name" is a key but if "name" key may change
+       so we use companion object in LINE 8 and instead of using "name" we change to "NAME_EXTRA"
+        And we have to change in both MainActivity and BirthdayActivity */
+
+        var name=intent.getStringExtra("name_extra")
+        //activity_birthday_id.text="Happy BirthDay $name!"
+        var txtview : TextView=findViewById(R.id.activity_birthday_id)
+        txtview.text="Happy Birthday $name ..... !"
+
+
+    }
+
+
+}
