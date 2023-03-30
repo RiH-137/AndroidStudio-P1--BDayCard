bc742d55ebeaf86b84bdc6898c331d2c24d39012
Rishi Ranajan <67317269+RiH-137@users.noreply.github.com>
2023-03-30 22:45:32 +0530

    Update BirthdayActivity.kt

--- BirthdayActivity.kt
+++ BirthdayActivity.kt
@@ -7,20 +7,20 @@ import android.widget.TextView
 class BirthdayActivity : AppCompatActivity() {
     companion object{                                   //companion object just create a static variable
 
-      const val NAME_EXTRA ="name_extra"
+        const val NAME_EXTRA ="name_extra"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday)
 
-       /* In general we use -->  val name  =intent.getStringExtra("name") <-- and here "name" is a key but if "name" key may change
-       so we use companion object in LINE 8 and instead of using "name" we change to "NAME_EXTRA"
-        And we have to change in both MainActivity and BirthdayActivity */
+        /* In general we use -->  val name  =intent.getStringExtra("name") <-- and here "name" is a key but if "name" key may change
+        so we use companion object in LINE 8 and instead of using "name" we change to "NAME_EXTRA"
+         And we have to change in both MainActivity and BirthdayActivity */
 
         val name=intent.getStringExtra("name_extra")
         //activity_birthday_id.text="Happy BirthDay $name!"
         var txtview : TextView=findViewById(R.id.activity_birthday_id)
-        txtview.text="Happy Birthday $name ..... !"
+        txtview.text="Happy Birthday ${intent.getStringExtra(name)} ..... !"
 
 
     }
